ce62f5d7a66c53df103209bf25b5ccf611f92659
ngalandebright-alt <ngalandebright1@gmail.com>
2026-08-20 15:31:50 +0200

    Propare backend for production deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,38 +1,33 @@
 require("dotenv").config();
 
-console.log("Mongo URI:", process.env.MONGO_URI);
-
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
+
 const transactionRoutes = require("./routes/transactionRoutes");
 const authRoutes = require("./routes/authRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
-
-
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
-app.use("/api/transactions", transactionRoutes)
+app.use("/api/transactions", transactionRoutes);
 app.use("/api/budgets", budgetRoutes);
 
-
 app.get("/", (req, res) => {
-    res.json({ 
-        massage: "Finance Manager API is running"
-});
+  res.json({
+    message: "Finance Manager API is running",
+  });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}` );
+  console.log(`Server running on port ${PORT}`);
 });
